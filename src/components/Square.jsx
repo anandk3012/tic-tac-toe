155c829cf48a929748babbb0615fa41c7965ac0b
anandk3012 <anandkash99@gmail.com>
2024-06-15 01:17:10 +0530

    some changes

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from 'react'
 
-const Square = ({ value, onClick }) => {
+export default function Square({value,onClick}) {
   return (
-    <div
-      className='flex items-center justify-center w-1/3 p-3 border-lg border-l-gray-950'
-      onClick={onClick}
-      style={{ cursor: 'pointer', width: '100px', height: '100px', border: '1px solid black' }}
+    <div 
+    className='flex items-center justify-center w-1/3 p-3 border-lg border-l-gray-950'
+    onClick={onClick}
+    style={{ cursor: 'pointer', width: '100px', height: '100px', border: '1px solid black' }}
     >
-      <h1 className='text-3xl text-bold '>{value}</h1>
+      <h1 className="text-2xl text-bold">
+        {value}
+      </h1>
     </div>
-  );
-};
-
-export default Square;
+  )
+}
